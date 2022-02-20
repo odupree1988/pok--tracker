4cff762ae6866ac308b261a42496a103a0ae1ae2
Owen Dupree <odupree1988@gmail.com>
2022-02-20 00:35:56 -0500

    create card function added bootstrap added

--- assets/script.js
+++ assets/script.js
@@ -41,24 +41,56 @@ async function iteratePokémon(data) {
         }
       })
       .then((data) => {
-        console.log(data);
         pokéList.push(data);
 
-        const divEl = document.createElement("div");
-        const imageEl = document.createElement("img");
-
-        for (let i = 0; i < pokéList.length; i++) {
-          let pokémonImg = pokéList[i].sprites.front_default;
-
-          imageEl.setAttribute("id", "pokémon-image");
-          imageEl.setAttribute("src", pokémonImg);
-
-          divEl.append(imageEl);
-          pokémonImageEl.append(divEl);
-        }
+        createCard(pokéList);
       });
   }
 }
+
+createCard = (data) => {
+  gridEl = document.createElement("div");
+  cardEl = document.createElement("div");
+  imgEl = document.createElement("img");
+  cardBodyEl = document.createElement("div");
+  pEl = document.createElement("p");
+
+  for (let i = 0; i < data.length; i++) {
+    let pokémonImg = data[i].sprites.other["official-artwork"].front_default;
+
+    let cardAttributes = [
+      {
+        class: "card",
+        style: "width: 18rem",
+      },
+      {
+        class: "card-img-top",
+        src: pokémonImg,
+        alt: "",
+      },
+    ];
+
+    gridEl.setAttribute("class", "col-xs-4 m-1");
+    setAttributes(cardEl, cardAttributes[0]);
+    setAttributes(imgEl, cardAttributes[1]);
+    cardBodyEl.setAttribute("class", "card-body");
+    pEl.setAttribute("class", "card-text");
+    pEl.innerHTML =
+      data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1);
+
+    cardBodyEl.append(pEl);
+    cardEl.append(imgEl);
+    cardEl.append(cardBodyEl);
+    gridEl.append(cardEl);
+    pokémonImageEl.append(gridEl);
+  }
+};
+
+setAttributes = (element, attributes) => {
+  Object.keys(attributes).forEach((attr) => {
+    element.setAttribute(attr, attributes[attr]);
+  });
+};
 
 nextEl.addEventListener("click", () => {
   pokémonImageEl.innerHTML = "";
